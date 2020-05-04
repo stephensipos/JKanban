5bda724c89dfe68b4c108723b642389a5073f1ce
Istvan Sipos <ccsistvan@cctk.org>
2020-05-04 23:20:45 +0200

    Lingering thread miatt System.exit(0);, java14 -> java11

--- src/main/java/com/stephensipos/jkanban/model/dao/GenericDao.java
+++ src/main/java/com/stephensipos/jkanban/model/dao/GenericDao.java
@@ -4,5 +4,5 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class GenericDao {
-    protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jkanban-mariadb");
+    public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jkanban-mariadb");
 }
